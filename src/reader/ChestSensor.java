58c4dcf261adb645d41956e983959ad0321d880b
Ricky <rs79c@mail.missouri.edu>
2015-02-12 16:35:54 -0600

    Draft Version to generate data without filtering

--- src/reader/ChestSensor.java
+++ src/reader/ChestSensor.java
@@ -13,65 +13,162 @@ import java.util.LinkedList;
 
 public class ChestSensor {
 
+	public static final String PATH = "C:/Users/Ricky/Desktop/Example/";
+	// public static final String SUFFIX = ".txt";
+	public static final String SUFFIX = ".csv";
+	public static final String HEAD = "time,motion,body position,BR derived by Belt,HR derived by ECG,Belt Quality,ECG Quality,HR confidence,BR confidence,Day";
+	public final String LINEBREAK = System.getProperty("line.separator");
+	private String wFileName;
+
 	public ChestSensor() {
+		wFileName = PATH + "Process" + SUFFIX;
 	}
 
 	public ChestSensor(String fileName) {
+		wFileName = PATH + fileName + SUFFIX;
 	}
 
-	private int convertPosition(String position) {
-		int rtn = -1;
+	private String convertMotion(String position) {
+		String rtn = "";
 		switch (position) {
 		case "Stationary":
-			rtn = 10;
+			rtn = "1";
+			break;
 		case "MovingSlowly":
-			rtn = 40;
+			rtn = "2";
+			break;
 		case "MovingFast":
-			rtn = 70;
+			rtn = "3";
+			break;
 		}
 		return rtn;
 	}
 
-	private int convertMotion(String motion) {
-		int rtn = -1;
+	private String convertPosition(String motion) {
+		String rtn = "";
 		switch (motion) {
 		case "Inverted":
-			rtn = 10;
+			rtn = "10";
+			break;
 		case "Prone":
-			rtn = 20;
+			rtn = "20";
+			break;
 		case "Side":
-			rtn = 30;
+			rtn = "30";
+			break;
 		case "Supine":
-			rtn = 40;
+			rtn = "40";
+			break;
 		case "Unknown":
-			rtn = 50;
+			rtn = "50";
+			break;
 		case "Upright":
-			rtn = 60;
+			rtn = "60";
+			break;
 		}
 		return rtn;
+	}
+
+	private void splitFilesByTime() {
+		BufferedReader br;
+		String data;
+		StringBuffer sb = new StringBuffer();
+		String sTime = "";
+		String pTime = "";
+		String eTime = "";
+		String day = "";
+		int count = -1;
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(wFileName)));
+			// BufferedWriter out = new BufferedWriter(new FileWriter(wFileName,
+			// true));
+			while ((data = br.readLine()) != null) {
+				String[] tmp = data.trim().split(",");
+				if (!tmp[0].equals("time")) {
+					count++;
+					if (count == 0) {
+						sTime = tmp[0];
+						pTime = tmp[0];
+						day = tmp[9];
+						sb.append(data).append(LINEBREAK);
+						eTime = pTime;
+						// System.out.println("time1: " + pTime);
+					} else {
+						pTime = eTime;
+						eTime = tmp[0];
+						System.out.println("timeP_" + count + ": " + pTime);
+						System.out.println("timeE_" + count + ": " + eTime);
+						if (!compareTime(pTime, eTime)) {
+							String fName = PATH + day + sTime.substring(0, 2) + "-" + eTime.substring(0, 2) + "_" + SUFFIX;
+							String toWrite = sb.toString();
+							writeMethod(fName,toWrite);
+							System.out.println("count: " + count);
+							pTime = "";
+							eTime = "";
+							sTime = "";
+							count = -1;
+							sb.setLength(0);
+						}
+						sb.append(data).append(LINEBREAK);
+					}
+				}
+			}
+			if (!sb.toString().equals("")) {
+				String fName = PATH + day + sTime.substring(0, 2) + "-" + eTime.substring(0, 2) + "_" + SUFFIX;
+				String toWrite = sb.toString();
+				writeMethod(fName, toWrite);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 *
 	 * @param rFileName
 	 * @param wFileName
-	 *            file: 0: time; 1: motion; 2: position; 3: BR; 4:
-	 *
+	 *            file:
+	 *            0: time; 1: motion; 2: position; 3: BR; 4: HR; 5: Belt
+	 *            Quality;
+	 *            6: ECG Quality; 7: HR Confidence; 8: BR Confidence, 9:Day
 	 * @return
 	 */
-	private boolean processChestData(String rFileName, String wFileName) {
+	private boolean simpleProcessChestData(String rFileName) {
 		BufferedReader br;
+		String data;
+		String toWrite;
 		try {
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(rFileName)));
-			BufferedWriter out = new BufferedWriter(new FileWriter(wFileName, true));
-			String data = null;
+			// BufferedWriter out = new BufferedWriter(new FileWriter(wFileName,
+			// true));
+			BufferedWriter out = new BufferedWriter(new FileWriter(wFileName));
+			out.write(HEAD);
+			out.newLine();
 			while ((data = br.readLine()) != null) {
 				String[] tmp = data.trim().split(",");
 				if (!tmp[0].equals("time")) {
-					// TODO: losts of dealing
+					// check BR/HR Confidence &HR value
+					if (Double.parseDouble(tmp[8]) <= 0.4 || Double.parseDouble(tmp[3]) == 0.0) {
+						tmp[3] = "N/A";
+					}
+					if (Double.parseDouble(tmp[7]) <= 0.4 || Double.parseDouble(tmp[4]) >= 180 || Double.parseDouble(tmp[4]) <= 40) {
+						tmp[4] = "N/A";
+					}
+					tmp[1] = convertMotion(tmp[1]);
+					tmp[2] = convertPosition(tmp[2]);
+					toWrite = getStrTime(tmp[0]).concat(",").concat(tmp[1]).concat(",").concat(tmp[2]).concat(",")
+							.concat(tmp[3]).concat(",").concat(tmp[4]).concat(",").concat(tmp[5]).concat(",")
+							.concat(tmp[6]).concat(",").concat(tmp[7]).concat(",").concat(tmp[8]).concat(",")
+							.concat(getDMYDate(tmp[0]));
+					out.write(toWrite);
+					out.newLine();
 				}
 			}
 			br.close();
+			out.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -82,6 +179,53 @@ public class ChestSensor {
 			return false;
 		}
 		return true;
+	}
+
+	public static void writeMethod(String fileName, String toWrite) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+			out.write(HEAD);
+			out.newLine();
+			out.write(toWrite);
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private String getDMYDate(String date) {
+		String mon = date.substring(4, 7);
+		String day = date.substring(8, 10);
+		String year = date.substring(24);
+		return day + "_" + mon + "_" + year;
+	}
+
+	private String getStrTime(String date) {
+		String time = date.substring(11, 19);
+		return time;
+	}
+
+	private int getCalTime(String time) {
+		String h = time.substring(0, 2);
+		String m = time.substring(3, 5);
+		String s = time.substring(6, 8);
+		return Integer.parseInt(h) * 3600 + Integer.parseInt(m) * 60 + Integer.parseInt(s);
+	}
+
+	/**
+	 * Currently set 1 minute as Threshold
+	 *
+	 * @param sTime
+	 * @param eTime
+	 * @return
+	 */
+	private boolean compareTime(String sTime, String eTime) {
+		if (getCalTime(eTime) - getCalTime(sTime) >= 60) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	private LinkedList<LinkedList<String>> getChestData(String rFileName) throws IOException {
@@ -105,7 +249,10 @@ public class ChestSensor {
 		return processedData;
 	}
 	public static void main(String[] args) throws IOException {
-		ChestSensor _chestSensor = new ChestSensor();
-		_chestSensor.getChestData("C:/Users/Ricky/Desktop/Example/chestsensor.1001.EQ02_0913006.O_09.txt");
+		String _rFileName = "chestsensor.9999.EQ02_3112228.F_10.txt";
+		ChestSensor _chestSensor = new ChestSensor(_rFileName);
+//		_chestSensor.getChestData(_rFileName);
+		_chestSensor.simpleProcessChestData(PATH + _rFileName);
+		_chestSensor.splitFilesByTime();
 	}
 }
--- src/reader/Test.java
+++ src/reader/Test.java
@@ -1,21 +1,62 @@
 package reader;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Test {
-	public static void main(String[] args) {
-		// String s = "-39.0";
-		// double d = Double.parseDouble(s);
-		// System.out.println(d);
-		ArrayList<Integer> aList = new ArrayList<Integer>(2);
-		aList.add(1);
-		aList.add(2);
-		aList.add(3);
+	public static String Date = "Thu Oct 09 08:28:46 CDT 2014";
 
-		Iterator<Integer> it = aList.iterator();
-		while (it.hasNext()) {
-			System.out.println(it.next());
+	public static String convertPosition(String position) {
+		String rtn = "";
+		switch (position) {
+		case "Stationary":
+			rtn = "10";
+		case "MovingSlowly":
+			rtn = "40";
+		case "MovingFast":
+			rtn = "70";
 		}
+		return rtn;
+	}
+
+	private String getStrTime(String date) {
+		String time = date.substring(11, 19);
+		return time;
+	}
+
+	private static int getCalTime(String time) {
+		String h = time.substring(0, 2);
+		String m = time.substring(3, 5);
+		String s = time.substring(6, 8);
+		return Integer.parseInt(h) * 3600 + Integer.parseInt(m) * 60 + Integer.parseInt(s);
+	}
+	public static String getDMYDate(String date) {
+		String Mon = date.substring(4, 7);
+		String Day = date.substring(8, 10);
+		String Year = date.substring(24);
+		String time = date.substring(11, 20);
+		String h = date.substring(11, 13);
+		String m = date.substring(14, 16);
+		String s = date.substring(17, 19);
+
+		return Day + "/" + Mon + "/" + Year + " " + h + "," + m + "," + s;
+	}
+
+	public static boolean compareTime(String sTime, String eTime) {
+		if (getCalTime(eTime) - getCalTime(sTime) >= 5 * 60) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	public static void main(String[] args) {
+		// System.out.print(getDMYDate(Date));
+		// System.out.print(getCalTime("08:28:46"));
+		// System.out.println(compareTime("08:28:46", "08:30:51"));
+		// System.out.println(compareTime("08:28:46", "08:38:51"));
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("s");
+		sb.setLength(0);
+		System.out.print(!sb.toString().equals(""));
+
 	}
 }
